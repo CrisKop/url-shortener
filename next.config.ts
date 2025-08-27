81a978d4649d112baae46820b6f8172707086364
Cristian Prince <62496847+CrisKop@users.noreply.github.com>
2025-08-27 00:42:28 -0500

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,3 +1,9 @@
 import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {};
+const nextConfig: NextConfig = {
+  images: {
+    domains: ["www.google.com"], // <— agrega todos los dominios que quieras usar
+  },
+};
+
+module.exports = nextConfig;
